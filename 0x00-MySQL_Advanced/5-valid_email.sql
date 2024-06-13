fa888edac9f35e0165a294ca4ec6c56a4267d984
Sphax <itssphax123@gmail.com>
2024-06-13 06:45:23 -0700

    5. Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- 5-valid_email.sql
+DELIMITER $$
+CREATE TRIGGER reset
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END $$
+DELIMITER;
