0720c53575fd91118a338c464997686fc72f37eb
SphaxWare <itssphax123@gmail.com>
2024-06-18 16:36:14 -0700

    10. Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Change school topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update with py
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
